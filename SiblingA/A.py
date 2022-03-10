1ca4fe4e157570393d073064d4fdc29933b1f3f5
valhuber <valjhuber@gmail.com>
2022-03-09 17:48:38 -0800

    running example

---
+++ SiblingA/A.py
@@ -0,0 +1,14 @@
+# import requi9red module
+import sys
+
+# append the path of the
+# parent directory
+sys.path.append("..")
+
+# import method from sibling
+# module
+from SiblingB.B import methodB
+
+# call method
+s = methodB()
+print(s)
---
+++ run.py
@@ -0,0 +1,10 @@
+from SiblingB.B import methodB
+
+def main():
+    # call method
+    s = methodB()
+    print(s)
+
+if __name__ == "__main__":
+    print(f'\n run.py, starting')
+    main()
